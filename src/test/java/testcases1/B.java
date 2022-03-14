f8fa09d9920f730e0701466fd9a5ae96e2d0c625
unknown <pirdankarprasad@gmail.com>
2022-03-14 19:00:26 +0530

    New programs addes

---
+++ src/test/java/testcases1/B.java
@@ -0,0 +1,13 @@
+package testcases1;
+
+import java.util.Arrays;
+
+public class B {
+	public static void main(String [] args) {
+		int ar[]= {10,40,70,80,90,101,102,105,107,79,85,58,65,45,87};
+		Arrays.sort(ar);
+		System.out.println(ar[ar.length-1]);
+		
+		
+	}
+}
